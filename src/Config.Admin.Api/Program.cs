5ecef6cb572b788227f361133b4df6592621abe5
Paw <paw.baltzersen@fragt.dk>
2022-12-02 10:02:46 +0100

    Show swagger page in non-dev environment

--- src/Config.Admin.Api/Program.cs
+++ src/Config.Admin.Api/Program.cs
@@ -29,11 +29,8 @@ builder.Services.AddScoped<IParser, Parser>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowOrigin");
