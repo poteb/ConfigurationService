00101dcd413c744efc7fa7b5b943c3851490b6c1
Paw <paw.baltzersen@fragt.dk>
2022-12-21 22:44:05 +0100

    Implemented IEquatable in ConfigEnvironment

--- src/Config.Admin.WebClient/Config.Admin.WebClient/Model/ConfigEnvironment.cs
+++ src/Config.Admin.WebClient/Config.Admin.WebClient/Model/ConfigEnvironment.cs
@@ -1,15 +1,27 @@
 ﻿namespace pote.Config.Admin.WebClient.Model;
 
-public class ConfigEnvironment
+public class ConfigEnvironment : IEquatable<ConfigEnvironment>
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = string.Empty;
 
     public bool IsDeleted { get; set; }
     public bool IsSelected { get; set; }
+    
+    public bool Equals(ConfigEnvironment? other)
+    {
+        if (object.ReferenceEquals(other, null)) return false;
+        if (object.ReferenceEquals(this, other)) return true;
+        return Id.Equals(other.Id) && Name.Equals(other.Name);
+    }
 
     public override string ToString()
     {
         return Name;
+    }
+    
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode() ^ Name.GetHashCode();
     }
 }
