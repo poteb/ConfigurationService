e7ee85e6358668a2fb7dbd940f8375e80270a9c8
Paw <paw@pote.dk>
2023-01-15 21:30:53 +0100

    Show usages for environments (#80)

--- src/Config.Admin.WebClient/Config.Admin.WebClient/Pages/Environments.razor.cs
+++ src/Config.Admin.WebClient/Config.Admin.WebClient/Pages/Environments.razor.cs
@@ -6,11 +6,13 @@ namespace pote.Config.Admin.WebClient.Pages;
 
 public partial class Environments
 {
+    private bool _usageLoaded;
+    
     public List<ConfigEnvironment> List { get; set; } = new();
-
     [Inject] public IAdminApiService AdminApiService { get; set; } = null!;
-    //[CascadingParameter] public PageError PageError { get; set; } = null!;
     [CascadingParameter] public PageError PageError { get; set; } = null!;
+    [Inject] public IDependencyGraphApiService DependencyGraphApiService { get; set; } = null!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = null!;
 
     protected override async Task OnInitializedAsync()
     {
@@ -42,5 +44,43 @@ public partial class Environments
     private void AddItem()
     {
         List.Add(new ConfigEnvironment());
+    }
+    
+    private async Task LoadUsages()
+    {
+        try
+        {
+            var callResponse = await DependencyGraphApiService.GetDependencyGraph();
+
+            if (!callResponse.IsSuccess)
+            {
+                PageError.OnError(callResponse.GenerateErrorMessage(), new Exception());
+                return;
+            }
+
+            if (callResponse.Response is not {})
+            {
+                PageError.OnError("Response was empty", new Exception());
+                return;
+            }
+
+            foreach (var environment in List)
+            {
+                environment.Usages.Clear();
+                var usages = callResponse.Response.Edges.Where(e => e.ToId == environment.Id);
+                environment.Usages.AddRange(usages);
+            }
+            
+            _usageLoaded = true;
+        }
+        catch (Exception ex)
+        {
+            PageError.OnError("Error loading usages", ex);
+        }
+    }
+    
+    private void GoToUsage(string id)
+    {
+        NavigationManager.NavigateTo($"EditConfiguration/{id}", new NavigationOptions { ForceLoad = false });
     }
 }
