67ba33754be88aca85530cf06c8341d3dd8ceb01
Paw <paw@pote.dk>
2023-08-28 10:34:00 +0200

    Added DateTime from Header to Configuration when saving (#115)
    Order by DateTime descending when getting history.
    Replaced Moq with NSubstitude.

---
+++ src/Config.UnitTests/DateTimeParserTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace pote.Config.UnitTests;
+
+[TestFixture]
+public class DateTimeParserTests
+{
+    [Test]
+    public async Task TestWinterDateTimeUtc()
+    {
+        var dataProvider = new TestDataProvider();
+        var parser = new Parser.Parser(dataProvider);
+        var response = await parser.Parse("{\"MyDateTime\":\"2022-12-07T09:25:13.585Z\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        var obj = JsonConvert.DeserializeObject<DateTimeContainerClass>(response);
+        Assert.AreEqual(new DateTime(2022, 12, 7, 9, 25, 13, 585, DateTimeKind.Utc), obj?.MyDateTime);
+        var timeZoneId = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+        var local = TimeZoneInfo.ConvertTimeFromUtc(obj.MyDateTime, timeZoneId);
+        Assert.AreEqual(new DateTime(2022, 12, 7, 10, 25, 13, 585, DateTimeKind.Local), local);
+    }
+    
+    [Test]
+    public async Task TestSummerDateTimeUtc()
+    {
+        var dataProvider = new TestDataProvider();
+        var parser = new Parser.Parser(dataProvider);
+        var response = await parser.Parse("{\"MyDateTime\":\"2023-08-22T11:56:11.151Z\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        var obj = JsonConvert.DeserializeObject<DateTimeContainerClass>(response);
+        Assert.AreEqual(new DateTime(2023, 8, 22, 11, 56, 11, 151, DateTimeKind.Utc), obj?.MyDateTime);
+        var timeZoneId = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+        var local = TimeZoneInfo.ConvertTimeFromUtc(obj.MyDateTime, timeZoneId);
+        Assert.AreEqual(new DateTime(2023, 8, 22, 13, 56, 11, 151, DateTimeKind.Local), local);
+    }
+}
+
+public class DateTimeContainerClass
+{
+    public DateTime MyDateTime { get; set; }
+}
--- src/Config.UnitTests/ParserTests.cs
+++ src/Config.UnitTests/ParserTests.cs
@@ -2,51 +2,49 @@ using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using pote.Config.Shared;
 
-namespace pote.Config.UnitTests
+namespace pote.Config.UnitTests;
+
+[TestFixture]
+public class ParserTests
 {
-    [TestFixture]
-    public class ParserTests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+    }
 
-        [Test]
-        public async Task TestBasic()
-        {
-            var dataProvider = new TestDataProvider();
-            var parser = new Parser.Parser(dataProvider);
-            var response = await parser.Parse("{\"Value1\":\"$ref:Wagga#Wagga\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
-            var dyn = JsonConvert.DeserializeObject<dynamic>(response);
-            var value1 = dyn?.Value1.ToString();
-            Assert.AreEqual("Mama", value1);
-            response = await parser.Parse("{\"Value1\":\"$ref:Wagga#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
-            dyn = JsonConvert.DeserializeObject<dynamic>(response);
-            value1 = dyn?.Value1.Wagga.ToString();
-            Assert.AreEqual("Mama", value1);
-        }
+    [Test]
+    public async Task TestBasic()
+    {
+        var dataProvider = new TestDataProvider();
+        var parser = new Parser.Parser(dataProvider);
+        var response = await parser.Parse("{\"Value1\":\"$ref:Wagga#Wagga\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        var dyn = JsonConvert.DeserializeObject<dynamic>(response);
+        var value1 = dyn?.Value1.ToString();
+        Assert.AreEqual("Mama", value1);
+        response = await parser.Parse("{\"Value1\":\"$ref:Wagga#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        dyn = JsonConvert.DeserializeObject<dynamic>(response);
+        value1 = dyn?.Value1.Wagga.ToString();
+        Assert.AreEqual("Mama", value1);
+    }
 
-        [Test]
-        public async Task TestNested()
-        {
-            var dataProvider = new TestDataProvider();
-            var parser = new Parser.Parser(dataProvider);
-            var response = await parser.Parse("{\"Value1\":\"$ref:Wagga_nested#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
-            var dyn = JsonConvert.DeserializeObject<dynamic>(response);
-            var value1 = dyn?.Value1.Wagga.Super.ToString();
-            Assert.AreEqual("mule", value1);
-        }
+    [Test]
+    public async Task TestNested()
+    {
+        var dataProvider = new TestDataProvider();
+        var parser = new Parser.Parser(dataProvider);
+        var response = await parser.Parse("{\"Value1\":\"$ref:Wagga_nested#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        var dyn = JsonConvert.DeserializeObject<dynamic>(response);
+        var value1 = dyn?.Value1.Wagga.Super.ToString();
+        Assert.AreEqual("mule", value1);
+    }
 
-        [Test, Explicit]
-        public async Task CircularReference_ShouldNot_StackOverflow()
-        {
-            var dataProvider = new TestDataProvider();
-            var parser = new Parser.Parser(dataProvider);
-            var response = await parser.Parse("{\"Wagga\":\"$ref:Circular#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
-            var dyn = JsonConvert.DeserializeObject<dynamic>(response);
-        }
+    [Test, Explicit]
+    public async Task CircularReference_ShouldNot_StackOverflow()
+    {
+        var dataProvider = new TestDataProvider();
+        var parser = new Parser.Parser(dataProvider);
+        var response = await parser.Parse("{\"Wagga\":\"$ref:Circular#\"}", "unittest", "test", _ => { }, CancellationToken.None, "");
+        var dyn = JsonConvert.DeserializeObject<dynamic>(response);
     }
 }
